29cf9163bdeaa8f61d56bba44fcae68768bb0466
Duhoon <eclip6@naver.com>
2024-03-30 16:06:12 +0900

    feat: config, db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.RDS_HOST,
+      username: process.env.RDS_USER,
+      password: process.env.RDS_PASSWORD,
+      database: 'data',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
